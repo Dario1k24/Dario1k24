f92bfe06046c6426ed7e2fc2444027c66f31d7ba
Dario1k24 <dariodibella07@gmail.com>
2024-06-07 14:16:35 +0200

    essai

--- APP_FILMS_164/films/gestion_films_crud.py
+++ APP_FILMS_164/films/gestion_films_crud.py
@@ -14,6 +14,63 @@ from APP_FILMS_164.database.database_tools import DBconnection
 from APP_FILMS_164.erreurs.exceptions import *
 from APP_FILMS_164.films.gestion_films_wtf_forms import FormWTFUpdateFilm, FormWTFAddFilm, FormWTFDeleteFilm
 
+"""
+    Nom : films_genres_afficher
+    Auteur : OM 2021.05.01
+    Définition d'une "route" /films_genres_afficher
+
+    But : Afficher les films avec les genres associés pour chaque film.
+
+    Paramètres : id_genre_sel = 0 >> tous les films.
+                 id_genre_sel = "n" affiche le film dont l'id est "n"
+
+"""
+
+
+@app.route("/films_afficher/<int:id_film_sel>", methods=['GET', 'POST'])
+def films_afficher(id_film_sel):
+    if request.method == "GET":
+        try:
+            with DBconnection() as mc_afficher:
+                strsql_genres_films_afficher_data = """SELECT
+                                                        c.ID_fournisseur, p.ID_produit, c.nom_fournisseur, c.adresse_fournisseur, c.numero_telephone_fournisseur, p.nom_produit, p.description_produit, p.poids_produit, p.prix_produit
+                                                        FROM t_fournisseur c
+                                                        LEFT JOIN t_produit p ON c.ID_produit = p.ID_produit
+                                                        LEFT JOIN t_fournisseur_produit sc ON c.ID_fournisseur = sc.ID_fournisseur"""
+                if id_film_sel == 0:
+                    # le paramètre 0 permet d'afficher tous les films
+                    # Sinon le paramètre représente la valeur de l'id du film
+                    mc_afficher.execute(strsql_genres_films_afficher_data)
+                else:
+                    # Constitution d'un dictionnaire pour associer l'id du film sélectionné avec un nom de variable
+                    valeur_id_film_selected_dictionnaire = {"value_id_film_selected": id_film_sel}
+                    # En MySql l'instruction HAVING fonctionne comme un WHERE... mais doit être associée à un GROUP BY
+                    # L'opérateur += permet de concaténer une nouvelle valeur à la valeur de gauche préalablement définie.
+                    strsql_genres_films_afficher_data += """ HAVING ID_fournisseur= %(value_id_film_selected)s"""
+
+                    mc_afficher.execute(strsql_genres_films_afficher_data, valeur_id_film_selected_dictionnaire)
+
+                # Récupère les données de la requête.
+                data_genres_films_afficher = mc_afficher.fetchall()
+                print("data_genres ", data_genres_films_afficher, " Type : ", type(data_genres_films_afficher))
+
+                # Différencier les messages.
+                if not data_genres_films_afficher and id_film_sel == 0:
+                    flash("""La table "t_film" est vide. !""", "warning")
+                elif not data_genres_films_afficher and id_film_sel > 0:
+                    # Si l'utilisateur change l'id_film dans l'URL et qu'il ne correspond à aucun film
+                    flash(f"Le film {id_film_sel} demandé n'existe pas !!", "warning")
+                else:
+                    flash(f"Données films et genres affichés !!", "success")
+
+        except Exception as Exception_films_genres_afficher:
+            raise ExceptionFilmsGenresAfficher(f"fichier : {Path(__file__).name}  ;  {films_afficher.__name__} ;"
+                                               f"{Exception_films_genres_afficher}")
+
+    print("films_genres_afficher  ", data_genres_films_afficher)
+    # Envoie la page "HTML" au serveur.
+    return render_template("films/films_afficher.html", data=data_genres_films_afficher)
+
 """Ajouter un film grâce au formulaire "film_add_wtf.html"
 Auteur : OM 2022.04.11
 Définition d'une "route" /film_add
@@ -53,7 +110,7 @@ def film_add_wtf():
                 flash("Données insérées !!", "success")
                 print("Données insérées !!")
 
-                return redirect(url_for('films_genres_afficher'))
+                return redirect(url_for('fournisseur_produit_afficher'))
 
         except Exception as Exception_genres_ajouter_wtf:
             raise ExceptionGenresAjouterWtf(f"fichier : {Path(__file__).name}  ;  "
@@ -62,7 +119,7 @@ def film_add_wtf():
 
     return render_template("films/film_add_wtf.html", form_add_film=form_add_film)
 
-"""Editer(update) un film qui a été sélectionné dans le formulaire "films_genres_afficher.html"
+"""Editer(update) un film qui a été sélectionné dans le formulaire "fournisseur_produit_afficher.html"
 Auteur : OM 2022.04.11
 Définition d'une "route" /film_update
 
@@ -106,7 +163,7 @@ def film_update_wtf():
             flash("Donnée mise à jour !!", "success")
             print("Donnée mise à jour !!")
 
-            return redirect(url_for('films_genres_afficher'))
+            return redirect(url_for('fournisseur_produit_afficher'))
         elif request.method == "GET":
             str_sql_id_film = "SELECT * FROM t_fournisseur WHERE ID_fournisseur = %(value_id_film)s"
             valeur_select_dictionnaire = {"value_id_film": id_film_update}
@@ -130,7 +187,7 @@ def film_update_wtf():
     return render_template("films/film_update_wtf.html", form_update_film=form_update_film)
 
 
-"""Effacer(delete) un film qui a été sélectionné dans le formulaire "films_genres_afficher.html"
+"""Effacer(delete) un film qui a été sélectionné dans le formulaire "fournisseur_produit_afficher.html"
 Auteur : OM 2022.04.11
 Définition d'une "route" /film_delete
 
